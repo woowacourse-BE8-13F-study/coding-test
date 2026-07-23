c0c68c8c6f459d0a0786ac5c729435852b045440
YEJIN KIM <glow.yejin@gmail.com>
2026-07-23 21:58:12 +0900

    solve: 05-배열 두 개 뽑아서 더하기 (yejinkimis)

---
+++ solutions/05-배열/두개뽑아서더하기/yejinkimis.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        Set<Integer> answer = new HashSet<>();
+        for(int i = 0; i < numbers.length - 1; i++) {
+            for(int j = i + 1; j < numbers.length; j++) {
+                answer.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        return answer.stream().sorted().mapToInt(i -> i).toArray();
+    }
+}
